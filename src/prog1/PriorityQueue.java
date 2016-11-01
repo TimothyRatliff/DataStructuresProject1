b8e66fd323740b97db9ba71644709fa36c36b4fd
TimothyRatliff <timothykratliff@gmail.com>
2016-10-31 21:25:58 -0500

    almostdone

--- src/prog1/PriorityQueue.java
+++ src/prog1/PriorityQueue.java
@@ -1,33 +1,57 @@
 package prog1;
-
+//Programming Project 1
+//Timothy Ratliff tratli2@lsu.edu
 public class PriorityQueue {
-	int max, length;
-	Entry[] queue;
-	Entry x;
+	private int max, length;
+	private Entry[] queue;
 	
-	private class Entry{
+	/**
+	 * Creates an Entry with two fields; name and priority.
+	 * @author TimothyRatliff
+	 */
+	private class Entry
+	{
 		public String name;
 		public int priority;
 	}
 	
+	/**
+	 * Constructs a PriorityQueue with length zero, and a given size.
+	 * @param size The size of the PQ
+	 */
 	public PriorityQueue(int size)
-	   {
-	      max = size;
-	      queue = new Entry[max];
-	      length = 0;
-	   }
+    {
+		max = size;
+		queue = new Entry[max];
+		length = 0;
+    }
 	
-	public boolean isEmpty(){
+	/**
+	 * Determines if the given PQ is empty.
+	 * @return True or False
+	 */
+	public boolean isEmpty()
+	{
 		return (length == 0);
 	}
 	
+	/**
+	 * Inserts a value into the PQ with a given key
+	 * @param value The given value
+	 * @param key The given key
+	 */
 	public void insert(String value, int key)
-	   {
+    {
 		Entry x = new Entry();
 		x.name = value;
 		x.priority = key;
 		queue[length] = x;
-		int newpos = length;
+	    Up(length);
+	    length++;
+    }
+	
+	private void Up(int newpos)
+	{
 		Entry temp = queue[newpos];
 		int parent = (newpos-1)/2;
 		while(newpos > 0 && queue[parent].priority < temp.priority){
@@ -35,23 +59,51 @@ public class PriorityQueue {
 			newpos = parent;
 			parent = (newpos-1)/2;
 		}
-	      queue[newpos] = temp;
-	      length++;
-	   }
-	public Entry remove()
-	   {
-	      Entry temp = queue[0];
-	      length--;
-	      if(length > 0){
-	    	  queue[0] = queue[length];
-	      }
-	      return temp;
-	   }
-	public String toString(){
-		String str = "";
-        for(int i = 1; i <= length; i ++)
-            str += (queue[i].name);
-        return str;
+		queue[newpos] = temp;
 	}
 	
+	/**
+	 * Removes the Entry with the highest 
+	 * priority value, then prints and returns the name 
+	 * string from that record.
+	 * @return The Entry with the highest priority value.
+	 */
+	public String remove()
+    {
+		if (isEmpty()) return null;
+	    Entry temp = queue[0];
+	    length--;
+	    if(length > 0){
+	    	  queue[0] = queue[length];
+	    	  Down(0);
+	    }
+	    System.out.printf(temp.name + "%n");
+	    return temp.name;
+    }
+	
+	private void Down(int position)
+	{
+		int child;
+		if(2*position+1 >= length){
+			child = position;
+		}
+		else if (2*position+2 == length)
+	    {
+	        child = 2*position+1;     
+	    }
+	    else if (queue[2*position+1].priority > queue[2*position+2].priority)
+	    {
+	        child = 2*position+1;      
+	    }
+	    else
+	    {
+	        child = 2*position+2;      
+	    }
+		if(queue[position].priority < queue[child].priority){
+			Entry temp = queue[position];
+			queue[position] = queue[child];
+			queue[child] = temp;
+			Down(child);
+		}
+	}
 }
--- src/prog1/prog1demo.java
+++ src/prog1/prog1demo.java
@@ -1,17 +1,28 @@
 package prog1;
 //Programming Project 1
 //Timothy Ratliff tratli2@lsu.edu
+
+import java.io.*;
+import java.util.Scanner;
 public class prog1demo {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	public static void main(String[] args) throws FileNotFoundException{
+		Scanner input = new Scanner(new File(args[0]));
 		PriorityQueue queue = new PriorityQueue(255);
-		queue.insert("Walter", 21);
-		queue.insert("Matthew", 13);
-		queue.insert("Lisa", 12);
-		queue.insert("Richard", 17);
-		//queue.remove();
-		queue.toString();
+		while (input.hasNextLine()){
+			String str = input.next();
+			if(str.equals("insert"))
+			{
+				String value = input.next();
+				int key = input.nextInt();
+				queue.insert(value, key);
+			}
+			if(str.equals("remove"))
+			{
+				queue.remove();
+			}
+		}
+		input.close();
 	}
 
 }
